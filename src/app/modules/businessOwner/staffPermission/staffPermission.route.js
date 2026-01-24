6f8101997c75027db85a13934d2e679efae9cb16
ahanaf607307 <ahanaf607307@gmail.com>
2026-01-24 09:36:37 +0600

    complete staff permission by business owner

---
+++ src/app/modules/businessOwner/staffPermission/staffPermission.route.js
@@ -0,0 +1,21 @@
+
+import { Router } from "express";
+import { StaffPermissionController } from "./staffPermission.controller.js";
+import { checkAuthMiddleware } from "../../../middleware/checkAuthMiddleware.js";
+import { Role } from "../../../utils/role.js";
+
+const router = Router();
+
+router.post(
+    "/manage",
+    checkAuthMiddleware(Role.BUSINESS_OWNER),
+    StaffPermissionController.handleUpsertPermission
+);
+
+router.get(
+    "/manage/:businessId",
+    checkAuthMiddleware(Role.BUSINESS_OWNER),
+    StaffPermissionController.handleGetPermission
+);
+
+export const StaffPermissionRoutes = router;
