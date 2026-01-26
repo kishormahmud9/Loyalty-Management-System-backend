d4d01cceea78193160900346299fd1b7634aba4c
ahanaf607307 <ahanaf607307@gmail.com>
2026-01-26 10:55:40 +0600

    fix middleware issue for all role without customer

--- src/app/modules/businessOwner/branchs/branchs.route.js
+++ src/app/modules/businessOwner/branchs/branchs.route.js
@@ -9,7 +9,7 @@ import { PERMISSIONS } from "../../../config/permissions.js";
 const router = Router()
 
 router.post('/create', authenticate, authorize(PERMISSIONS.BRANCH.CREATE), businessScope, BranchController.create)
-router.get('/', authenticate, authorize(PERMISSIONS.BRANCH.READ), businessScope, BranchController.findAll)
+router.get('/all/:businessId', authenticate, authorize(PERMISSIONS.BRANCH.READ), businessScope, BranchController.findAll)
 router.get('/:id', authenticate, authorize(PERMISSIONS.BRANCH.READ), businessScope, BranchController.findOne)
 router.put('/:id', authenticate, authorize(PERMISSIONS.BRANCH.UPDATE), businessScope, BranchController.update)
 router.delete('/:id', authenticate, authorize(PERMISSIONS.BRANCH.DELETE), businessScope, BranchController.delete)
