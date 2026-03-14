cfb079c4fe2c3109d6a3361f2b14d74076e719f6
NKRifat47 <nk.rifat47@gmail.com>
2026-03-15 01:14:49 +0600

    requirePinSetup logic fixed

--- src/app/modules/staff/auth/staff.auth.service.js
+++ src/app/modules/staff/auth/staff.auth.service.js
@@ -55,7 +55,7 @@ export const staffLoginService = async (prisma, payload) => {
 
     return {
       token,
-      requirePinSetup: user.isPinSet,
+      requirePinSetup: !user.isPinSet,
       branchId: user.staffProfile.branchId,
     };
   } catch (error) {
